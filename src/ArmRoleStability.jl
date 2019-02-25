4b6bfd259fab82afba42e550a5c0d97b1b9c3f10
Allen Hill <allenofthehills@gmail.com>
2019-02-25 16:38:19 -0500

    Files generated by PkgTemplates

---
+++ src/ArmRoleStability.jl
@@ -0,0 +1,5 @@
+module ArmRoleStability
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ArmRoleStability
+using Test
+
+@testset "ArmRoleStability.jl" begin
+    # Write your own tests here.
+end
